c51ee668ab67e6bc1b181cb12cc3b6f6d80fafe0
DylanMMP <35112899+DylanMMP@users.noreply.github.com>
2021-09-13 10:18:07 -0500

    Update Map.js

--- react-native-app/components/Map.js
+++ react-native-app/components/Map.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
-const Map = (props) => {
-	return (
-		<View style={props.windowStyle}>
-			<Text>This is ideally the map.</Text>
-		</View>
-	);
-}
+import React, { useState } from 'react';
+import { View, StyleSheet,Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+const App = () => {
+const imageURL = ''
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 33.2083,
+    longitude: -87.5504,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={mapRegion}
+      >
+            <Marker
+          coordinate={{latitude: 33.2083, longitude: -87.5504}}
+          title="this is a marker"
+          description="this is a marker example"
+        />
+        </MapView>
+    </View>
+  );
+};
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Map;
