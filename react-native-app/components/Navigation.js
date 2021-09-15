437b962d9442ce7a35158c90d351a4957b8ed218
ldpresley1 <90281601+ldpresley1@users.noreply.github.com>
2021-09-14 22:04:05 -0500

    Update Navigation.js
    Fixed the issue with the buttons resizing on the map. Resized the icons and nav bar to be more proportional.

--- react-native-app/components/Navigation.js
+++ react-native-app/components/Navigation.js
@@ -4,16 +4,16 @@ import { Pressable, Image, StyleSheet, Text, View } from 'react-native';
 const Navigation = (props) => {
   return (
 	<View style={styles.navigation}>
-		<Pressable onPress={() => props.setPage(0)} style={ props.page == 0 ? styles.button : [styles.button,{backgroundColor:'#FFFFFFC5'}]}>
+		<Pressable onPress={() => props.setPage(0)} style={ props.page == 0 ? styles.button : [styles.button,{backgroundColor:'#B3E8FC'}]}>
 			<Image style={styles.buttonIcon} source={require('../assets/addicon.png')} />
 		</Pressable>
-		<Pressable onPress={() => props.setPage(1)} style={ props.page == 1 ? styles.button : [styles.button,{backgroundColor:'#FFFFFFC5'}]}>
+		<Pressable onPress={() => props.setPage(1)} style={ props.page == 1 ? styles.button : [styles.button,{backgroundColor:'#B3E8FC'}]}>
 			<Image style={styles.buttonIcon} source={require('../assets/mapicon.png')} />
 		</Pressable>
-		<Pressable onPress={() => props.setPage(2)} style={ props.page == 2 ? styles.button : [styles.button,{backgroundColor:'#FFFFFFC5'}]}>
+		<Pressable onPress={() => props.setPage(2)} style={ props.page == 2 ? styles.button : [styles.button,{backgroundColor:'#B3E8FC'}]}>
 			<Image style={styles.buttonIcon} source={require('../assets/clockicon.png')} />
 		</Pressable>
-		<Pressable onPress={() => props.setPage(3)} style={ props.page == 3 ? styles.button : [styles.button,{backgroundColor:'#FFFFFFC5'}]}>
+		<Pressable onPress={() => props.setPage(3)} style={ props.page == 3 ? styles.button : [styles.button,{backgroundColor:'#B3E8FC'}]}>
 			<Image style={styles.buttonIcon} source={require('../assets/pfp.png')} />
 		</Pressable>
 	</View>
@@ -25,9 +25,9 @@ export default Navigation;
 const styles = StyleSheet.create({
 	navigation: {
 		width: "100%",
-		// position:'absolute',
-		// bottom: 0,
-		flex:1,
+		position:'absolute',
+		bottom: 0,
+		//flex:1,
 		backgroundColor: '#F47174',
 		flexDirection: "row",
 	},
@@ -39,11 +39,11 @@ const styles = StyleSheet.create({
 		// borderRightWidth: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingBottom: 40,
-		paddingTop: 20
+		paddingBottom: 15,
+		paddingTop: 15
 	},
 	buttonIcon: {
-		width: 50,
-		height: 50,
+		width: 40,
+		height: 40,
 	},
 });
