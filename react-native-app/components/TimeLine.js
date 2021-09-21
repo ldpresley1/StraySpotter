03ce6fd559f94f36e13e70e4912a575d4ce471ab
Asa Dillahunty <stantondillahunty@gmail.com>
2021-09-21 13:20:31 -0500

    :round_pushpin: Opens in Maps, Added Tags
    Posts now have a maps icon that will open the stored location of the post in the default maps app. Note: this requires the expo-linking dependency. Removing this feature removes the need for this dependency
    
    Posts now have tags.
    
    TODO:
    - need to connect to database to work with the real fake data

--- react-native-app/components/TimeLine.js
+++ react-native-app/components/TimeLine.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet, ScrollView, Image, Dimensions, Appearance } from 'react-native';
+import { Text, View, FlatList, StyleSheet, ScrollView, Image, Dimensions, Appearance, Pressable } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { darkTheme, lightTheme } from './Themes';
+import * as Linking from 'expo-linking';
 
 const theme = Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme
 const screenWidth = Dimensions.get('window').width;
@@ -11,55 +13,73 @@ const DATA = [
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
 		title: 'First Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/04/30/Pictures/_70e9d6ee-8af0-11ea-8bae-d48e751bd032.jpeg','https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/04/30/Pictures/_70e9d6ee-8af0-11ea-8bae-d48e751bd032.jpeg','https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Calico','Orange','Medium','Young','Collar','Multilingual']
 	},
 	{
 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
 		title: 'Second Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d72',
 		title: 'Third Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		
+		images: ['https://upload.wikimedia.org/wikipedia/commons/f/f5/Sketch_Of_My_Dog_.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Dog','Brown','Medium']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d73',
 		title: 'Forth Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Tabby','Green','Business Savvy']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d74',
 		title: 'Fifth Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','A Child','Orange','Smol']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d75',
 		title: 'Sixth Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Calico','Orange','Medium']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d76',
 		title: 'Seventh Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Calico','Orange','Medium']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d77',
 		title: 'Eighth Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Calico','Orange','Medium']
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d78',
 		title: 'Ninth Item',
 		description: "This is the post's description. This is where you'll put details about the animal.",
-		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png']
+		images: ['https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/cat-behaviour.jpg','https://pyxis.nymag.com/v1/imgs/424/858/e6c66c3a1992e711bca0137b754fea749f-cat-law.rsquare.w700.jpg','https://images-na.ssl-images-amazon.com/images/I/71%2BmDoHG4mL.png'],
+		cord: {lat:33.209953358934264,long:-87.5463168},
+		tags: ['Cat','Calico','Orange','Medium']
 	},
 ];
 
@@ -121,15 +141,41 @@ class Post extends React.Component {
 					)
 					: <></>
 				}
-				<Text style={styles.title}> {this.props.title} </Text>
+				<View style={styles.postHeader}>
+					<Text style={styles.title}> {this.props.title} </Text>
+					<Pressable onPress={() => openMap(this.props.cord)}>
+						<Icon style={styles.iconStyle} name='map-marker-alt' type='fontisto' color={theme.colors.foreground}/>
+					</Pressable>
+				</View>
 				<Text style={styles.description}>{this.props.description}</Text>
+				<View style={styles.tagContainer}>
+					{
+						this.props.tags.map((tagText, index) => {
+							return <Text key={index} style={styles.tag}>{tagText}</Text>
+						})
+					}
+				</View>
 			</View>
 		)
 	}
 }
 
+// this is copied directly from stack overflow
+function openMap(cord) {
+	const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+	const latLng = `${cord.lat},${cord.long}`;
+	const label = 'Custom Label';
+	const url = Platform.select({
+		ios: `${scheme}${label}@${latLng}`,
+		android: `${scheme}${latLng}(${label})`
+	});
+
+		
+	Linking.openURL(url);
+}
+
 const renderPost = ({ item }) => (
-	<Post description={item.description} title={item.title} images={item.images} />
+	<Post description={item.description} title={item.title} images={item.images} cord={item.cord} tags={item.tags}/>
 );
 
 const TimeLine = (props) => {
@@ -161,7 +207,7 @@ const styles = StyleSheet.create({
 	post: {
 		width: screenWidth,
 		flex: 1,
-		paddingBottom:theme.spacing.l,
+		paddingBottom:theme.spacing.m,
 		overflow:'hidden'
 	},
 	scrollViewContainer: {
@@ -194,11 +240,22 @@ const styles = StyleSheet.create({
 		margin: 5,
 		color: 'white'
 	},
+	postHeader: {
+		flex:1,
+		flexDirection:'row',
+		justifyContent: "space-between",
+		alignItems:'center',
+	},
 	title: {
 		fontSize: 32,
 		textDecorationLine: 'underline',
 		paddingTop:theme.spacing.s,
+		marginLeft:theme.spacing.m,
 		color:theme.colors.foreground
+	},
+	iconStyle: {
+		marginRight:theme.spacing.m,
+		// backgroundColor:'red'
 	},
 	description: {
 		...theme.textVariants.body,
@@ -206,4 +263,18 @@ const styles = StyleSheet.create({
 		paddingBottom:theme.spacing.s,
 		color:theme.colors.foreground
 	},
+	tagContainer: {
+		flex:1,
+		flexDirection:'row',
+		flexWrap:'wrap'
+	},
+	tag: {
+		backgroundColor:theme.colors.background,
+		color:theme.colors.foreground,
+		padding:theme.spacing.s,
+		marginHorizontal:theme.spacing.s,
+		marginVertical:2,
+		borderRadius:10,
+		overflow:'hidden'
+	}
 });
