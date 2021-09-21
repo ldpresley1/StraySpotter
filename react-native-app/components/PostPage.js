24f7d62bfd21c7dbd0d4b4e2b8277ffb94c3c643
ldpresley1 <90281601+ldpresley1@users.noreply.github.com>
2021-09-20 20:36:45 -0500

    Fixed Issues.
    Added more dropdowns and fixed various issues. Added the library for text input.

--- react-native-app/components/PostPage.js
+++ react-native-app/components/PostPage.js
@@ -1,11 +1,9 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useState, useCallback} from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 
 const PostPage = (props) => {
-	const [open, setOpen] = useState(false);
-const [value, setValue] = useState(null);
 const [types, setTypes] = useState([
   {label: 'Dog', value: 'Dog'},
   {label: 'Cat', value: 'Cat'}
@@ -14,32 +12,116 @@ const [Breeds, setBreeds] = useState([
 	{label: 'figure out how to switch between', value: 'apple'},
 	{label: 'Cat and dog breeds', value: 'banana'}
   ]);
+  const [Color, setColors] = useState([
+	{label: 'Brown', value: 'Brown'},
+	{label: 'Tan', value: 'Tan'},
+	{label: 'White', value: 'White'},
+	{label: 'Black', value: 'Black'},
+	{label: 'Orange', value: 'Orange'},
+	{label: 'Grey', value: 'Grey'}
+  ]);
+  const [Sizes, setSizes] = useState([
+	{label: 'Small', value: 'Small'},
+	{label: 'Medium', value: 'Medium'},
+	{label: 'Large', value: 'Large'},
+	{label: 'Huge', value: 'Huge'}
+  ]);
+  const [typeOpen, settypeOpen] = useState(false);
+  const [breedOpen, setbreedOpen] = useState(false);
+  const [colorOpen, setcolorOpen] = useState(false);
+  const [sizeOpen, setsizeOpen] = useState(false);
+
+  const[typeValue, settypeValue] = useState(null);
+  const[breedValue, setbreedValue] = useState(null);
+  const[colorValue, setcolorValue] = useState([]);
+  const[sizeValue, setsizeValue] = useState(null);
+
+  const ontypeOpen = useCallback(() => {
+    setbreedOpen(false);
+	setcolorOpen(false);
+	setsizeOpen(false);
+  }, []);
+
+  const onbreedOpen = useCallback(() => {
+    settypeOpen(false);
+	setcolorOpen(false);
+	setsizeOpen(false);
+  }, []);
+  const oncolorOpen = useCallback(() => {
+    settypeOpen(false);
+	setbreedOpen(false);
+	setsizeOpen(false);
+  }, []);
+  const onsizeOpen = useCallback(() => {
+    settypeOpen(false);
+	setbreedOpen(false);
+	setcolorOpen(false);
+  }, []);
 	return (
 		
-		<View style={[props.windowStyle, styles.container]}>
+		<View style={[styles.container]}>
 		<>
 		<DropDownPicker
-      open={open}
+      open={typeOpen}
+	  onOpen={ontypeOpen}
 	  defaultNull
 	  placeholder = "Type"
-      value={value}
+      value={typeValue}
       items={types}
-      setOpen={setOpen}
-      setValue={setValue}
+      setOpen={settypeOpen}
+      setValue={settypeValue}
       setItems={setTypes}
-	  containerStyle={{ width: 82, height: 100}}
+	  containerStyle={{width:82}}
+	  zIndex={4000}
+      zIndexInverse={1000}
+    />
+	<DropDownPicker
+      open={colorOpen}
+	  onOpen={oncolorOpen}
+	  defaultNull
+	  placeholder = "Colors"
+      value={colorValue}
+      items={Color}
+      setOpen={setcolorOpen}
+      setValue={setcolorValue}
+      setItems={setColors}
+	  multiple={true}
+      min={0}
+      max={6}
+	  containerStyle={{width:125}}
+	  zIndex={3000}
+      zIndexInverse={1000}
+    />
+	<DropDownPicker
+      open={sizeOpen}
+	  onOpen={onsizeOpen}
+	  defaultNull
+	  placeholder = "Size"
+      value={sizeValue}
+      items={Sizes}
+      setOpen={setsizeOpen}
+      setValue={setsizeValue}
+      setItems={setSizes}
+	  containerStyle={{width:93}}
+	  zIndex={2000}
+      zIndexInverse={1000}
     />
            <DropDownPicker
-      open={open}
+      open={breedOpen}
+	  onOpen={onbreedOpen}
 	  defaultNull
 	  placeholder = "Breed"
 	  searchable={true}
-      value={value}
+      value={breedValue}
       items={Breeds}
-      setOpen={setOpen}
-      setValue={setValue}
+	  setOpen={setbreedOpen}
+      setValue={setbreedValue}
+	  //disabled={true}
+	  disabledStyle={{opacity:0}}
+	  containerStyle={{width:200}}
       setItems={setBreeds}
-	  containerStyle={{ width: 120, height: 60}}
+	  zIndex={1000}
+      zIndexInverse={2000}
     />     
             </>
 			</View>
@@ -50,7 +132,12 @@ export default PostPage;
 
 const styles = StyleSheet.create({
 	container: {
-		//width,
-		height: 0
+		alignContent: 'space-around',
+		width: '100%',
+		flex: 6,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		marginLeft: 20,
+		marginRight: 20,
 	  },
 });
