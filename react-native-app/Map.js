b02747e588fd52b478cf03511e198656fc046f0c
DylanMMP <35112899+DylanMMP@users.noreply.github.com>
2021-09-23 12:41:57 -0500

    changed from png to jpg

---
+++ react-native-app/Map.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Dimensions, View, StyleSheet, Image, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
+
+const Map = (props) => {
+const imageURL = ''
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 33.2083,
+    longitude: -87.5504,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+
+  return (
+    <View style={[props.windowStyle, styles.container]}>
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={mapRegion}
+        showsUserLocation = {true}
+      >
+              <Marker
+            coordinate={{latitude: 33.2083, longitude: -87.5504}}
+            title="this is a marker"
+            description="this is a marker example"
+          >
+
+          <Image source={require('./images/whisper.jpg')} style={{height: 35, width:35 }} />
+          </Marker>
+        </MapView>
+    </View>
+  );
+};
+export default Map;
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height
+  },
+});
